0c18f1feed9b06c1048371484e288fad625da487
Tito Anugerah Maharizky <tito.anugerah@epson.co.id>
2022-05-23 17:08:27 +0700

    WWWROOT : Remove unused assets

---
+++ Cosmos/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿function notify(inputTitle = "Notification", inputMessage = null, inputType = "Secondary") {
+	$.notify({
+		icon: "flaticon-alarm-1",
+		title: inputTitle,
+		message: inputMessage,
+	}, {
+		type: inputType,
+		placement: {
+			from: "bottom",
+			align: "right"
+		},
+		time: 1000,
+	});
+}
