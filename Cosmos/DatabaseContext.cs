3120f9c74b2e67948828e9aa4d7376b3c2c25572
titoanugerah <titoanugerah@student.undip.ac.id>
2022-05-28 03:37:34 +0700

    DatabaseContext : Configure DB Context

--- Cosmos/DatabaseContext.cs
+++ Cosmos/DatabaseContext.cs
@@ -4,5 +4,31 @@ namespace Cosmos
 {
     public class DatabaseContext : DbContext
     {
+        private readonly IConfiguration config;
+        private readonly ILoggerFactory loggerFactory;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public DatabaseContext (IConfiguration _config, ILoggerFactory _loggerFactory, IHttpContextAccessor _httpContextAccessor)
+        {
+            config = _config;
+            loggerFactory = _loggerFactory;
+            httpContextAccessor = _httpContextAccessor;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+            optionsBuilder.EnableSensitiveDataLogging();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
     }
 }
